ccad554164881837c32736f79ce344df915b294f
eunjoo jung <lynn0506@snu.ac.kr>
2020-09-26 16:17:09 +0900

    typo update

--- eunjoo/sorting/boj3649.py
+++ eunjoo/sorting/boj3649.py
@@ -11,7 +11,7 @@ while True:
         # 일치하는 레고조각 2개를 찾는다. O(N)
         # 두 조각을 합쳐서 정답보다 작다면 src => +1, 크다면 des => -1 
         while src < des:
-            sum = legos[src] +두legos[des] 
+            sum = legos[src] +legos[des] 
             if sum == x:       
                 ans = "yes " + str(min(legos[src], legos[des])) \
                         + str(" ") +  str(max(legos[src], legos[des]))
